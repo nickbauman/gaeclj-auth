b21e0212b62e257b4afc0764a12fe29a5f6d1a02
nickbauman <nick.bauman@cirrusmachina.com>
2023-10-23 11:49:00 -0500

    upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
 (def appengine-version "2.0.20")
 
-(defproject gaeclj-auth "0.1.0"
+(defproject gaeclj-auth "0.1.1"
   :description "gaeclj-auth provides clojure helpers for auth API in Google App Engine"
-  :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [org.clojure/data.json "0.2.6"]
+  :javac-options ["-target" "1.11" "-source" "1.11" "-Xlint:-options"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [org.clojure/data.json "2.4.0"]
                  [ch.qos.logback/logback-classic "1.1.8"]
-                 [com.google.guava/guava "20.0"]
+                 [com.google.guava/guava "32.1.3-jre"]
                  [com.google.appengine/appengine-api-1.0-sdk ~appengine-version]
-                 [org.apache.httpcomponents/httpclient "4.5.2"]       
-                 [com.google.api-client/google-api-client-appengine "1.22.0"
+                 [org.apache.httpcomponents/httpclient "4.5.14"]
+                 [com.google.api-client/google-api-client-appengine "2.2.0"
                   :exclusions [com.google.guava/guava-jdk5]]
-                 [com.google.oauth-client/google-oauth-client-appengine "1.22.0"
+                 [com.google.oauth-client/google-oauth-client-appengine "1.34.1"
                   :exclusions [com.google.guava/guava-jdk5]]       
-                 [com.google.http-client/google-http-client-appengine "1.22.0"
+                 [com.google.http-client/google-http-client-appengine "1.43.3"
                   :exclusions [com.google.guava/guava-jdk5]]]
   :java-source-paths ["src-java"]
   :aot :all
